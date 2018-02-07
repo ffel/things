9aee5a30559b4d5a2fe9f1f039996069a9f5e0f4
Chris van Uffelen <chris.vanuffelen@han.nl>
2018-02-07 08:40:01 +0100

    header files of lesson 1

---
+++ les01/content.c
@@ -0,0 +1,9 @@
+#include "content.h"
+#include <stdint.h>
+#include <stdio.h>
+
+void hoi() {
+  for (int8_t i = 0; i < 3; i++) {
+    printf("%d: %s\n", i, "hallo klas!");
+  }
+}
---
+++ les01/content.h
@@ -0,0 +1,6 @@
+#ifndef content_h
+#define content_h
+
+void hoi();
+
+#endif
